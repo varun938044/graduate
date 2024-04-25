91f5ef7d480ccf038ab3683b18e8438eb4057d2b
varun938044 <167070149+varun938044@users.noreply.github.com>
2024-04-25 09:52:31 +0530

    binary tree

---
+++ levelordertraversal.py
@@ -0,0 +1,75 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data 
+        self.left = None 
+        self.right = None
+
+        
+def printLevelOrder(root):
+    result = []
+    Q = [root]
+ 
+    while len(Q) > 0:
+        n = len(Q)
+        subResult = []
+        for i in range(n):
+            # step-1: popping
+            curr = Q.pop(0)
+            subResult.append(curr.data)
+ 
+            # step-2: pushing left child if its not none 
+            if curr.left != None:
+                Q.append(curr.left)
+ 
+            # step-3: pushing right child if its not none
+            if curr.right != None:
+                Q.append(curr.right)
+ 
+        result.append(subResult)
+    for subLevel in result:
+        print(*subLevel)
+ 
+
+    
+    
+       
+
+        
+root = TreeNode(11)
+
+obj2 = TreeNode(7)
+obj3 = TreeNode(5)
+obj4 = TreeNode(3)
+obj5 = TreeNode(9)
+obj6 = TreeNode(8)
+obj7 = TreeNode(10)
+obj8 = TreeNode(15)
+obj9 = TreeNode(13)
+obj10 = TreeNode(12)
+obj11= TreeNode(14)
+obj12= TreeNode(20)
+obj13= TreeNode(18)
+obj14= TreeNode(25)
+ 
+
+root.left = obj2 
+root.right = obj8
+ 
+obj2.left = obj3
+obj2.right = obj5 
+ 
+obj3.left = obj4
+
+obj5.left = obj6
+obj5.right = obj7
+
+obj8.left = obj9
+obj8.right = obj12
+
+obj9.left = obj10
+obj9.right = obj11
+
+obj12.left = obj13
+obj12.right = obj14
+
+printLevelOrder(root)
