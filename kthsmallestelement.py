4be809ec5c41c6c7df5d852bf6e2345b86ed6495
varun938044 <167070149+varun938044@users.noreply.github.com>
2024-04-26 09:27:33 +0530

    bst

---
+++ kthsmallestelement.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.right =   None
+        self.left = None
+
+n=int(input())
+num=list(map(int,input().split()))
+k=int(input())
+
+def fillInorder(inorder,root):
+    if root==None:
+        return
+    fillInorder(inorder,root.left)
+    inorder.append(root.data)
+    fillInorder(inorder,root.right)
+    
+def insertIntoBST(root,ele):
+    if root==None:
+        new=Node(ele)
+        return new
+    if root.data<ele:
+        root.right=insertIntoBST(root.right,ele)
+    else:
+        root.left=insertIntoBST(root.left,ele)
+    return root
+
+root=None
+for ele in num:
+    root=insertIntoBST(root,ele)
+    
+inorder=[]
+fillInorder(inorder,root)
+print(inorder[k-1])
